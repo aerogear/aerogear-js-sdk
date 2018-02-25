e35f56db081ad765204d98a0ea2c9abe99f03f7f
Wojciech Trocki <wtrocki@redhat.com>
2018-02-25 10:58:56 +0000

    [Experiment] Wrap metrics code into specalized Cordova class

--- packages/metrics-cordova/src/index.ts
+++ packages/metrics-cordova/src/index.ts
@@ -1,1 +1,6 @@
-export * from '@aerogearservices/metrics';
+
+// WIP - Test why its not working properly.
+// export * from '@aerogearservices/metrics';
+
+// Export cordova wrapper
+export * from './MetricsService';
