3b4f9a385d56eec887430f179852189e5f438348
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-14 12:04:35 +0100

    partial commit

---
+++ example/react-native/src/components/HttpScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HttpScreen = () => {
+  return (
+    <View>
+      <Text>Http!</Text>
+    </View>
+  );
+};
+
+export default HttpScreen;
