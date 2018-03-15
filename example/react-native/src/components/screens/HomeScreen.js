0e702003388a3c1e9959e25b91896c3711ab9cf9
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-15 17:16:46 +0100

    set status bar's dark color

--- example/react-native/src/components/screens/HomeScreen.js
+++ example/react-native/src/components/screens/HomeScreen.js
@@ -22,7 +22,7 @@ class HomeScreen extends Component {
 
     return (
       <View style={styles.containerStyle}>
-        <StatusBar barStyle="light-content" />
+        <StatusBar barStyle="light-content" backgroundColor={Colors.primaryDark} />
         <View>
           <Text style={styles.welcomeStyle}>
             HOME
