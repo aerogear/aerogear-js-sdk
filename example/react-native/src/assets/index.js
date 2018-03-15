067c385fd60cc2d47d2294d7dc15b5691b239c0e
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-15 14:44:27 +0100

    adds styling to toolbar and statusbar

--- example/react-native/src/assets/index.js
+++ example/react-native/src/assets/index.js
@@ -3,4 +3,11 @@ const Images = {
   navDrawerIcon: require("./img/ic_nav_drawer_menu/ic_nav_drawer_menu.png"),
 };
 
-export { Images };
+const Colors = {
+  primary: "#d32f2f",
+  primaryDark: "#23242d",
+  accent: "#c62828",
+  white: "#ffffff"
+};
+
+export { Images, Colors };
