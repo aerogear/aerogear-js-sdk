08bb09c4b7ff1eeb48cea4c1184130dec3ec0b4c
Jose Miguel Gallas <josemigallas@gmail.com>
2018-04-06 14:58:10 +0200

    adds parser tests

--- packages/core/test/configuration/ConfigurationParser.ts
+++ packages/core/test/configuration/ConfigurationParser.ts
@@ -1,19 +1,52 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import mocha from "mocha";
-import { ConfigurationParser, ServiceConfiguration } from "../../src/configuration";
-import testAerogearConfig from "../mobile-config.json";
+import { AeroGearConfiguration, ConfigurationParser, ServiceConfiguration } from "../../src/configuration";
+import testConfigJson from "../mobile-config.json";
 
 describe("ConfigurationParser", () => {
 
-  let testSubject: ConfigurationParser;
+  const aerogearConfig = testConfigJson as AeroGearConfiguration;
+  let parser: ConfigurationParser;
 
   beforeEach(() => {
-    testSubject = new ConfigurationParser(testAerogearConfig);
+    parser = new MockConfigurationParser(aerogearConfig);
   });
 
+  describe("#constructor", () => {
+
+    it("should throw if configuration is null", () => {
+      const constructor = () => new MockConfigurationParser(null);
+      expect(constructor).to.throw();
+    });
+
+    it("should throw if configuration is undefined", () => {
+      const constructor = () => new MockConfigurationParser(undefined);
+      expect(constructor).to.throw();
+    });
+
+    it("should not instantiate null configurations", () => {
+      const emptyParser = new MockConfigurationParser({} as AeroGearConfiguration);
+      const configurations = emptyParser.getConfigurations();
+
+      assert.isArray(configurations);
+    });
+
+    it("should instantiate an array of configurations from a mobile-config JSON", () => {
+      const services = testConfigJson.services;
+      const configurations = (parser as MockConfigurationParser).getConfigurations();
+
+      assert.isArray(configurations);
+      assert.equal(configurations, services);
+    });
   });
 
   describe("#getService", () => {
+
+    it("should return undefined if using an nonexistent key", () => {
+      const result = parser.getConfig("foo" as any);
+
+      assert.isUndefined(result);
+    });
 
     it("should be able to get keycloak config", () => {
       const keycloakConfig = parser.getConfig("keycloak");
@@ -28,5 +61,13 @@ describe("ConfigurationParser", () => {
     });
 
   });
+
+  class MockConfigurationParser extends ConfigurationParser {
+
+    public getConfigurations(): ServiceConfiguration[] {
+      return this.configurations;
+    }
+
+  }
 
 });
