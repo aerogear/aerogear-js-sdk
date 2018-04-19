3620a6b3fd213973964d0f7533a35e41e3b4f2f6
Paolo Bueno <paolohaji@gmail.com>
2018-04-18 23:55:08 -0300

    Update RN example and core module

--- packages/core/android/src/main/java/org/aerogear/mobile/js/core/rn/Core.java
+++ packages/core/android/src/main/java/org/aerogear/mobile/js/core/rn/Core.java
@@ -1,40 +1,37 @@
 package org.aerogear.mobile.js.core.rn;
 
-import android.content.pm.PackageManager.NameNotFoundException;
-
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 import org.aerogear.mobile.js.core.MobileCore;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Core extends ReactContextBaseJavaModule {
 
   public final String SERVICE_NAME = "MobileCore";
-  private MobileCore core;
+  private final MobileCore core;
+  private final ReactApplicationContext context;
 
+  public Core(ReactApplicationContext context) {
+    super(context);
+    this.context = context;
+    this.core = new MobileCore(context);
+  }
 
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-    if (action.equals("getAppAndDeviceMetrics")) {
-      try {
-        callbackContext.success(getAppAndDeviceMetrics());
-        return true;
-      } catch (Exception e) {
-        callbackContext.error(e.getMessage());
-        return false;
-      }
+  @ReactMethod
+  public void getAppAndDeviceMetrics(Promise promise) {
+    try {
+      final JSONObject metrics = this.core.getAppAndDeviceMetrics();
+      promise.resolve(metrics);
+    } catch (Exception e) {
+      promise.reject(e);
     }
-    return false;
   }
 
   @Override
-  public String getServiceName() {
+  public String getName() {
     return SERVICE_NAME;
-  }
-
-  public JSONObject getAppAndDeviceMetrics() throws NameNotFoundException, JSONException {
-    return this.core.getAppAndDeviceMetrics();
   }
 }
