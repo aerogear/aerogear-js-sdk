d629617c140e4ed850448ff414e8b00d12f454a2
Wojciech Trocki <wtrocki@redhat.com>
2018-05-01 12:35:47 +0100

    fix: Provide metrics promises

--- packages/core/src/metrics/platform/CordovaAppMetrics.ts
+++ packages/core/src/metrics/platform/CordovaAppMetrics.ts
@@ -1,16 +1,33 @@
 import { AppMetrics, Metrics } from "../model";
 
-// plugin's JS interface
-declare var aerogear: {
-  getAppMetrics(): Promise<AppMetrics>;
-};
+// tslint:disable-next-line:no-var-requires
+const version = require("../../../package.json").version;
+
+declare var cordova: any;
 
 export class CordovaAppMetrics implements Metrics {
 
   public identifier = "app";
 
+  /**
+   * Get app metrics, to be called after deviceReady event.
+   *
+   * It uses cordova-plugin-app-version to get the app version, the rest of the metrics are to be
+   * filled up by the app itself.
+   * @returns {Promise<AppMetrics>} A promise containing the app metrics
+   */
   public collect(): Promise<AppMetrics> {
-    // Get app metrics from plugin cordova-plugin-aerogear-metrics
-    return aerogear.getAppMetrics();
+    const app = cordova.getAppVersion;
+    return Promise.all([
+      app.getPackageName(),
+      app.getVersionNumber()
+    ])
+      .then(function(results) {
+        return {
+          appId: results[0],
+          appVersion: results[1],
+          sdkVersion: version
+        };
+      });
   }
 }
--- packages/core/src/metrics/platform/CordovaDeviceMetrics.ts
+++ packages/core/src/metrics/platform/CordovaDeviceMetrics.ts
@@ -1,16 +1,25 @@
 import { DeviceMetrics, Metrics } from "../model";
 
 // plugin's JS interface
-declare var aerogear: {
-  getDeviceMetrics(): Promise<DeviceMetrics>;
-};
+declare var device: any;
 
 export class CordovaDeviceMetrics implements Metrics {
 
   public identifier = "device";
 
+  /**
+   * Get device metrics, to be called after deviceReady event
+   *
+   * It uses cordova-plugin-device plugin.
+   * @returns {Promise<DeviceMetrics>} The device metrics
+   */
   public collect(): Promise<DeviceMetrics> {
-    // Get device metrics from plugin cordova-plugin-aerogear-metrics
-    return aerogear.getDeviceMetrics();
+    return new Promise(function(res, rej) {
+      res({
+        platform: device.platform,
+        platformVersion: device.version,
+        device: device.model
+      });
+    });
   }
 }
