c3ffed92b0ceaf76f754eb5d756d3c8b9e0caf5b
Daniel Passos <daniel@passos.me>
2018-11-12 10:56:58 -0200

    Add authMiddleware

--- packages/sync/src/links/LinksBuilder.ts
+++ packages/sync/src/links/LinksBuilder.ts
@@ -1,8 +1,8 @@
-import { ApolloLink, split } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { getMainDefinition } from "apollo-utilities";
-import { IDataSyncConfig } from "..";
-import { defaultWebSocketLink } from "./WebsocketLink";
+import {ApolloLink, concat, split} from "apollo-link";
+import {HttpLink} from "apollo-link-http";
+import {getMainDefinition} from "apollo-utilities";
+import {IDataSyncConfig} from "..";
+import {defaultWebSocketLink} from "./WebsocketLink";
 
 /**
  * Function used to build apollo link
@@ -20,8 +20,19 @@ export const defaultLinkBuilder: LinkChainBuilder =
     }
 
     const httpLink = new HttpLink({ uri: config.httpUrl });
+    const authMiddleware = new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer 8abfae4eb02a6140c0a20798433180a063fd7006"
+        }
+      });
+
+      return forward(operation);
+    });
+
     // TODO drop your links here
-    let compositeLink = ApolloLink.from([httpLink]);
+    let compositeLink = ApolloLink.from([concat(authMiddleware, httpLink)]);
 
     if (config.wsUrl) {
       const wsLink = defaultWebSocketLink({ uri: config.wsUrl });
