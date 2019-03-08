a0536330ba355ec351b2b3a61a72c84e65ca93b9
Austin Cunningham <aucunnin@redhat.com>
2019-03-08 11:45:44 +0000

    feat(app-security): Add an AppSecurity module to '@aerogear/Securtity' package (AEROGEAR-8664)
    feat(app-security): address linting issues (AEROGEAR-8664)
    
    feat(get-client-data): returning metricsPayload (AEROGEAR-8773)
    
    feat(get-device-data): removed a console.info and added a todo comment (AEROGEAR-8773)
    
    feat(get-device-data): Added clientID to the metricsPayload (AEROGEAR-8773)
    
    feat(get-client-data): removed outer object on return (AEROGEAR-8773)
    
    feat(clientInit): convert the payload to Init payload(AEROGEAR-8774)
    
    feat(post-client-data): Post the client data to the Mobile Security Service Init endpoint (AEROGEAR-8774)
    
    feat(post-client-data): remove unused comments (AEROGEAR-8774)
    
    feat(post-client-data): added axios to package.json addressing pr commits(AEROGEAR-8774)
    
    feat(post-device-data): some minor lint issues (AEROGEAR-8774)
    
    feat(app-security): refactor to decouple app-security logic from the core/metrics publisher (AEROGEAR-8911)
    
    feat(app-security): addressing pr comments(AEROGEAR-8911)

---
+++ packages/security/src/appSecurity/AppSecurity.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+import console from "loglevel";
+import {
+  ServiceConfiguration,
+  ConfigurationService,
+  MetricsBuilder,
+  DefaultMetricsBuilder,
+  isMobileCordova
+ } from "@aerogear/core";
+
+export interface AppSecurityOptions {
+   metricsBuilder?: MetricsBuilder;
+}
+
+export class AppSecurity {
+  private static readonly TYPE: string = "security";
+  private internalConfig: any;
+  private metricsBuilder: MetricsBuilder;
+
+  constructor(config: ConfigurationService, options?: AppSecurityOptions) {
+    if (!isMobileCordova()) {
+      console.error("Cordova Platform Not Detected. This module should not be used.");
+    }
+
+    this.metricsBuilder = (options && options.metricsBuilder) ? options.metricsBuilder : new DefaultMetricsBuilder();
+
+    const configuration = config.getConfigByType(AppSecurity.TYPE);
+    if (configuration && configuration.length > 0) {
+      const serviceConfiguration: ServiceConfiguration = configuration[0];
+      this.internalConfig = serviceConfiguration.config;
+      // use the configuration url in the from the incoming config file
+      this.internalConfig.url = serviceConfiguration.url;
+    } else {
+      console.warn("Security configuration is missing. The Security module will not work properly.");
+    }
+  }
+
+  /**
+   * client Init called on device start returns app security enable/disable data
+   */
+  public async clientInit() {
+    const defaultMetrics = this.metricsBuilder.buildDefaultMetrics();
+    const defaultMetricsPayload = await this.metricsBuilder.buildMetricsPayload("security", defaultMetrics);
+
+    const initPayload: {[key: string]: any; } = {
+      deviceId: defaultMetricsPayload.clientId,
+      appId: defaultMetricsPayload.data.app.appId,
+      deviceType: defaultMetricsPayload.data.device.platform,
+      deviceVersion: defaultMetricsPayload.data.device.platformVersion,
+      version: defaultMetricsPayload.data.app.appVersion
+    };
+
+    return axios.post(`${this.internalConfig.url}/api/init`, initPayload);
+  }
+
+  /**
+   * Return the config used for the Security server
+   */
+  public getConfig(): string[] {
+    return this.internalConfig;
+  }
+
+  /**
+   * Return true if config is present
+   */
+  public hasConfig(): boolean {
+    return !!this.internalConfig;
+  }
+}
--- packages/security/src/index.ts
+++ packages/security/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./deviceTrust";
 export * from "./metrics";
 export { SecurityService } from "./SecurityService";
+export * from "./appSecurity/AppSecurity";
