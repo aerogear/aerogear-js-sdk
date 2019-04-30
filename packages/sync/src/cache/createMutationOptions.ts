99828871c254e09e1b46c573dd34784b3738d9b0
Wojciech Trocki <wtrocki@redhat.com>
2019-04-30 18:01:37 +0200

    WIP cache updates

---
+++ packages/sync/src/cache/createMutationOptions.ts
@@ -0,0 +1,50 @@
+import { MutationOptions, OperationVariables } from "apollo-client";
+import { DocumentNode } from "graphql";
+import { CacheOperation } from "./CacheOperation";
+import { createOptimisticResponse } from "./createOptimisticResponse";
+import { type } from "os";
+
+/**
+* @param operation operation that is being performed (update)
+* @param typeName type that is going to be returned
+* @param idField name of id field (default:id)
+*/
+
+export const buildMutation = <T = OperationVariables>(
+  mutation: DocumentNode,
+  variables: VariablesInfo<T> | T,
+  operation: string,
+  typeName: string,
+  operationType: CacheOperation = CacheOperation.ADD,
+  idField: string = "id"): MutationOptions => {
+
+  const optimisticResponse = createOptimisticResponse(operation,
+    typeName,
+    variables,
+    operationType === CacheOperation.ADD);
+
+  let update;
+  if (operationType === CacheOperation.ADD) {
+    update = buildAddOperation(operation, typeName, idField);
+  }
+
+  if (operationType === CacheOperation.DELETE) {
+    update = buildDeleteOperation(operation, typeName, idField);
+  }
+
+  return { mutation, variables, optimisticResponse, update };
+};
+
+export const buildAddOperation = (typeName: string, operation: string, idField: string){
+
+  return (update: any) => {
+    return update;
+  };
+};
+
+export const buildDeleteOperation = (typeName: string, operation: string, idField: string){
+
+  return (update: any) => {
+    return update;
+  };
+};
