b20990b898375e2e7d6f8877909bc85a48081648
Wojciech Trocki <wtrocki@redhat.com>
2019-07-09 22:50:00 +0100

    fix: Actual code migration

--- packages/sync/src/links/LinksBuilder.ts
+++ packages/sync/src/links/LinksBuilder.ts
@@ -1,20 +1,22 @@
 import { ApolloLink, Operation } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import { RetryLink } from "apollo-link-retry";
-import { conflictLink, ObjectState } from "../conflicts";
+import { ConflictLink, ObjectState } from "offix-offline";
 import { DataSyncConfig } from "../config";
 import { createAuthLink } from "./AuthLink";
 import { AuditLoggingLink } from "./AuditLoggingLink";
 import { DefaultMetricsBuilder, MetricsBuilder } from "@aerogear/core";
-import { LocalDirectiveFilterLink } from "./LocalDirectiveFilterLink";
+import { LocalDirectiveFilterLink } from "offix-offline";
 import { createUploadLink } from "apollo-upload-client";
-import { isMutation, isOnlineOnly, isSubscription } from "../utils/helpers";
+import { isMutation, isOnlineOnly, isSubscription } from "offix-offline";
 import { defaultWebSocketLink } from "./WebsocketLink";
-import { OfflineLink } from "../offline/OfflineLink";
-import { NetworkStatus, OfflineMutationsHandler, OfflineStore } from "../offline";
-import { IDProcessor } from "../offline/procesors/IDProcessor";
-import { ConflictProcessor } from "../conflicts/ConflictProcesor";
-import { IResultProcessor } from "../offline/procesors/IResultProcessor";
+import { OfflineLink } from "offix-offline";
+import { NetworkStatus, OfflineMutationsHandler, OfflineStore } from "offix-offline";
+import { IDProcessor } from "offix-offline";
+import { ConflictProcessor } from "offix-offline";
+import { IResultProcessor } from "offix-offline";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { BaseLink } from "offix-offline";
 
 /**
  * Method for creating "uber" composite Apollo Link implementation including:
@@ -26,8 +28,9 @@ import { IResultProcessor } from "../offline/procesors/IResultProcessor";
  * - Audit logging
  * - File uploads
  */
-export const createDefaultLink = async (config: DataSyncConfig, offlineLink: ApolloLink) => {
-  let link = await defaultHttpLinks(config, offlineLink);
+export const createDefaultLink = async (config: DataSyncConfig, offlineLink: ApolloLink,
+                                        conflictLink: ApolloLink, cache: InMemoryCache) => {
+  let link = await defaultHttpLinks(config, offlineLink, conflictLink, cache);
   if (config.wsUrl) {
     const wsLink = defaultWebSocketLink(config, { uri: config.wsUrl });
     link = ApolloLink.split(isSubscription, wsLink, link);
@@ -41,13 +44,23 @@ export const createDefaultLink = async (config: DataSyncConfig, offlineLink: Apo
 export const createOfflineLink = async (config: DataSyncConfig, store: OfflineStore) => {
   const resultProcessors: IResultProcessor[] = [
     new IDProcessor(),
-    new ConflictProcessor(config.conflictStateProvider as ObjectState)
+    new ConflictProcessor(config.conflictProvider as ObjectState)
   ];
-  return new OfflineLink({
-    store,
+  return new OfflineLink(store, {
     listener: config.offlineQueueListener,
     networkStatus: config.networkStatus as NetworkStatus,
     resultProcessors
+  });
+};
+
+/**
+ * Create conflict link
+ */
+export const createConflictLink = async (config: DataSyncConfig) => {
+  return new ConflictLink({
+    conflictProvider: config.conflictProvider as ObjectState,
+    conflictListener: config.conflictListener,
+    conflictStrategy: config.conflictStrategy
   });
 };
 
@@ -60,26 +73,28 @@ export const createOfflineLink = async (config: DataSyncConfig, store: OfflineSt
  * - Error handling
  * - Audit logging
  */
-export const defaultHttpLinks = async (config: DataSyncConfig, offlineLink: ApolloLink): Promise<ApolloLink> => {
+export const defaultHttpLinks = async (config: DataSyncConfig, offlineLink: ApolloLink,
+                                       conflictLink: ApolloLink, cache: InMemoryCache): Promise<ApolloLink> => {
+
   // Enable offline link only for mutations and onlineOnly
   const mutationOfflineLink = ApolloLink.split((op: Operation) => {
     return isMutation(op) && !isOnlineOnly(op);
   }, offlineLink);
+  const baseLink = new BaseLink(config.conflictProvider as ObjectState);
+  const links: ApolloLink[] = [baseLink, mutationOfflineLink];
+  links.push(conflictLink);
   const retryLink = ApolloLink.split(OfflineMutationsHandler.isMarkedOffline, new RetryLink(config.retryOptions));
-  const localFilterLink = new LocalDirectiveFilterLink();
-  const links: ApolloLink[] = [mutationOfflineLink, retryLink, localFilterLink];
+  links.push(retryLink);
 
   if (config.auditLogging) {
     links.push(await createAuditLoggingLink());
   }
 
-  if (config.conflictStrategy) {
-    links.push(conflictLink(config));
-  }
-
   if (config.authContextProvider) {
     links.push(createAuthLink(config));
   }
+  const localFilterLink = new LocalDirectiveFilterLink();
+  links.push(localFilterLink);
 
   if (config.fileUpload) {
     links.push(createUploadLink({
