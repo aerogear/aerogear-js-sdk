4f9bfd0022128059aa3c1fe9bb18647aa7624b2d
Stephen Coady <scoady@redhat.com>
2019-07-11 16:47:53 +0100

    fix: re-export missing packages

--- packages/sync/src/index.ts
+++ packages/sync/src/index.ts
@@ -2,3 +2,5 @@ export * from "./OfflineClient";
 export * from "offix-offline";
 export * from "./config";
 export * from "./links";
+export * from "./ApolloOfflineClient";
+export * from "offix-cache";
